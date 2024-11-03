724bd1e0577e2702d12d6cca67906026abc03106
Peyton Nguyen <peytonn72@gmail.com>
2024-11-03 00:01:53 -0700

    add table

---
+++ app/components/data-table.tsx
@@ -0,0 +1,78 @@
+"use client"
+
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  RowData,
+  useReactTable
+} from "@tanstack/react-table"
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow
+} from "~/components/ui/table"
+
+interface DataTableProps<TData extends RowData> {
+  columns: ColumnDef<TData, any>[]
+  data: TData[]
+}
+
+export function DataTable<TData>({ columns, data }: DataTableProps<TData>) {
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel()
+  })
+
+  return (
+    <div className="max-w-4xl mt-4 mx-auto rounded-md border">
+      <Table>
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                  </TableHead>
+                )
+              })}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow
+                key={row.id}
+                data-state={row.getIsSelected() && "selected"}
+              >
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={columns.length} className="h-24 text-center">
+                No results.
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </div>
+  )
+}
--- app/components/side-bar.tsx
+++ app/components/side-bar.tsx
@@ -1,3 +1,31 @@
+import { ChartNoAxesGantt, Hash } from "lucide-react"
+import { Button } from "~/components/ui/button"
+
+const test = "test"
+
 export function SideBar() {
-  return <div>hi</div>
+  return (
+    <div className="flex flex-col w-60 border-r overflow-y-scroll">
+      <div className="flex items-center gap-2 p-3 bg-background">
+        <ChartNoAxesGantt />
+        <p className="text-xl font-semibold">Leetpals</p>
+      </div>
+      <Button className="m-4">Create Group</Button>
+      <p className="px-4 font-semibold">Groups</p>
+      <div className="flex flex-col p-2">
+        <Button className="justify-start" variant="ghost">
+          <Hash />
+          Georgia Tech
+        </Button>
+        <Button className="justify-start" variant="ghost">
+          <Hash />
+          Roommates
+        </Button>
+        <Button className="justify-start" variant="ghost">
+          <Hash />
+          Girlfriend
+        </Button>
+      </div>
+    </div>
+  )
 }
